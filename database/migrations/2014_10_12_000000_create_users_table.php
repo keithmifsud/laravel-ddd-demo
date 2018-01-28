4ffa1c2c704043a830a5a2672336333488fcdc76
Keith Mifsud <mifsud.k@gmail.com>
2018-01-28 03:42:52 +0000

    Added uuid field to users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->uuid('user_identifier')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
