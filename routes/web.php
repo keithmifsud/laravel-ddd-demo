5011524d9361b6c434311c1fc288b6f6995bdfce
Keith Mifsud <mifsud.k@gmail.com>
2018-01-30 04:46:28 +0000

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Auth::routes();
 
-// Auth routes
-Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('/register', 'Auth\RegisterController@registerMember')->name('register');
-
+// Authentication routes
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 
+// Password reset routes
+Route::post(
+    '/password/email',
+    'Auth\ForgotPasswordController@sendResetLinkEmail'
+)->name('password.email');
 
-// Dashboard
+Route::get(
+    '/password/reset',
+    'Auth\ForgotPasswordController@showLinkRequestForm'
+)->name('password.request');
+
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::get(
+    '/password/reset/{token}',
+    'Auth\ResetPasswordController@showResetForm'
+)->name('password.reset');
+
+// Registration routes.
+Route::get('/register',
+    'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register',
+    'Auth\RegisterController@registerMember')->name('register');
+
+// Dashboard routes
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get(
+    '/profile',
+    'Member\ProfileController@showProfileForm'
+)->name('profile');
+
+Route::post('/profile', 'Member\ProfileController@updateProfile');
